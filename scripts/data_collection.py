8dc2f1332ca295492481ed207c8aa1c89b6eeebf
PUBG_QUANT <115015619+phounkpe2004@users.noreply.github.com>
2025-11-14 09:53:29 +0100

    feat: add function to extract and clean ISO country codes from Wikipedia
    This commit adds the getISO_codes function, which retrieves a Wikipedia table of countries and their ISO codes. It cleans the data by selecting relevant columns, renaming them appropriately, and returning a pandas DataFrame containing country names along with their ISO-2 and ISO-3 codes.

--- scripts/data_collection.py
+++ scripts/data_collection.py
@@ -54,8 +54,63 @@ def get_landlockedCountries(url):
     data_countries["Coastline(km)"] = pd.to_numeric(data_countries["Coastline(km)"])
     
     return data_countries
+
+
+
+def getISO_codes(url):
+
+    """
+    Scrapes a Wikipedia table containing ISO country codes, cleans the data, 
+    and returns a structured DataFrame with each country's name and its corresponding ISO codes.
+
+    Parameters
+    ----------
+    url : str
+        The URL of the Wikipedia page containing the table of countries and their ISO codes.
+
+    Returns
+    -------
+    pandas.DataFrame
+        A cleaned DataFrame with the following columns:
+        - 'Pays': the name of each country
+        - 'ISO-2': the two-letter ISO country code
+        - 'ISO-3': the three-letter ISO country code
+    """
+
+    requests_text = requests.get(
+    url,
+    headers={"User-Agent": "Python for data science tutorial"}
+    ).content
+
+    page = bs4.BeautifulSoup(requests_text, "lxml")
+    iso_table= page.find('table')
+    table_body = iso_table.find('tbody')
+    rows = table_body.find_all('tr')
+
+    # Regroupement des données dans un dictionnaire 
+    dico_ISO = dict()
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        if len(cols) > 0 : 
+            dico_ISO[cols[0]] = cols[:5] # Je ne sélectionne que les collones relatives aux frontières maritimes
+
+   
+    # Structuration dans un dataframe en suite
+    data_ISO = pd.DataFrame.from_dict(dico_ISO,orient='index')
+    data_ISO = data_ISO.reset_index()
+
+    columns_to_drop = data_ISO.columns[[0,2,3]] # J'enlève les données de la CIA. Pas très pertinentes 
+    data_ISO = data_ISO.drop(columns=columns_to_drop,axis=1)
+    data_ISO = data_ISO.rename(columns={0: "Pays", 2: "ISO-2", 4: "ISO-3"})
     
+    return data_ISO
+    
+# iso_url = "https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes"
+# data = getISO_codes(iso_url)
+# print(data.head())
 
 # url_Landlocked = "https://en.wikipedia.org/wiki/List_of_countries_by_length_of_coastline"
 # data = get_landlockedCountries(url_Landlocked)
 # print(data.head(10))
+
