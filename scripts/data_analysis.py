ed528860de562a6f31026031f1a24a3b5666ee70
patrick <gbedossoujunior@gmail.com>
2025-11-20 15:49:53 +0100

    added the data_visualization folder to contain the code for visual effects

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -9,7 +9,7 @@ def check_missing_values(data,col):
     fin = data["date"].max()
     
     print(f"Le dataframe contient des données temporelles relatives à {num_countries} pays de {debut} à {fin}.")
-    print(f"Il y a {missing_vals_number} valeurs manquantes sur un total de {total_values} dans la base de données. Soit un ratio de {(missing_vals_number/total_values)*100:.2f}%.")
+    print(f"Il y a {missing_vals_number} valeurs manquantes sur un total de {total_values} dans la base de données. Soit un ratio de {(missing_vals_number/total_values)*100:.2f}% de valeurs manquantes dans la base de données.")
 
     
     
