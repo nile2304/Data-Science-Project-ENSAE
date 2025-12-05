d6a373a7ae7fc0b244ba2dc18c60faaeaee93321
patrick <gbedossoujunior@gmail.com>
2025-12-05 11:45:56 +0100

    feature: finished the cleaning algorithm for countries name.
    Added backup files for the various data in order to deal with API errors

--- scripts/data_visualization.py
+++ scripts/data_visualization.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+
 
 def plot_missing_values_per_year(data,col):
     """
@@ -162,3 +165,28 @@ def plot_PIB_top_quantile_countries(PIB_data,chosen_quantile=19):
     plt.show()
 
     print(f'Countries in the {chosen_quantile+1}th-decile: {data[data['quantiles'] == chosen_quantile]['country'].unique()}')
+    
+def visualize_economicPower_clusters():
+
+    # KMeans clustering
+    kmeans = KMeans(n_clusters=4, random_state=42)
+    weightCountry["cluster"] = kmeans.fit_predict(weightCountry[["avgWeightCountry"]])
+
+    print(weightCountry[weightCountry["cluster"] == 3])
+
+    # Sort clusters by weight (so 3 = very high, 2 = high, 1= low, 0 = very low)
+    cluster_order = weightCountry.groupby("cluster")["avgWeightCountry"].mean().sort_values().index
+    mapping = {cluster_order[0]: 0, cluster_order[1]: 1, cluster_order[2]: 2, cluster_order[3]: 3}
+    weightCountry["cluster"] = weightCountry["cluster"].map(mapping)
+
+    import plotly.express as px
+
+    fig = px.choropleth(
+        weightCountry,
+        locations="country",
+        locationmode="ISO-3",
+        color="cluster",
+        color_continuous_scale=["red", "orange", "blue", "green"],
+        title="World Classification Map by Economic Power"
+    )
+    fig.show()
